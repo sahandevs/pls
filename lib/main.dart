98de6ab623e36689c37b0190bfb463d3072b46dc
sahandevs <sahandevs@gmail.com>
2020-07-31 02:51:06 +0430

    add roboto font

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Roboto',
       ),
       home: MainLayout(),
     );
