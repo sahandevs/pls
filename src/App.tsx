844e5350a627499a1349c4bd91e5ddd988ac23b0
Sahandevs <sahandevs@gmail.com>
2020-09-03 18:38:08 +0430

    convert buttons to tab in main container

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,42 @@
 import React from "react";
-import { CssBaseline, Box, Button, Card } from "@material-ui/core";
+import {
+  CssBaseline,
+  Box,
+  Card,
+  AppBar,
+  Tab,
+  Tabs,
+} from "@material-ui/core";
 import { ExchangePage } from "./ExchangePage";
 import { SetupCurrencies } from "./SetupCurrencies";
 import { SetupExchangeRates } from "./SetupExchangeRates";
 import { DBContext, CreateOrGetDefaultDatabase } from "./data/DB";
 
-type Page = "exchange" | "setup-currencies" | "setup-exhange-rates";
-
 function App() {
-  const [page, setPage] = React.useState<Page>("exchange");
+  const [page, setPage] = React.useState(0);
   const db = React.useMemo(() => CreateOrGetDefaultDatabase(), []);
   return (
     <DBContext.Provider value={db}>
       <CssBaseline />
-      <Box
-        display={"flex"}
-        flexDirection={"column"}
-        paddingTop={3}
-        alignItems={"center"}
-      >
-        <Box display={"flex"} flexDirection={"row"}>
-          <Button
-            variant={page === "setup-currencies" ? "outlined" : "text"}
-            onClick={() => setPage("setup-currencies")}
-          >
-            {"Setup Currencies"}
-          </Button>
-          <Button
-            variant={page === "exchange" ? "outlined" : "text"}
-            onClick={() => setPage("exchange")}
-          >
-            {"Exchange"}
-          </Button>
-          <Button
-            variant={page === "setup-exhange-rates" ? "outlined" : "text"}
-            onClick={() => setPage("setup-exhange-rates")}
-          >
-            {"Setup Exchange Rates"}
-          </Button>
-        </Box>
-        <Box marginTop={5} flex={1} minWidth={"75%"}>
-          <Card>
-            {page === "exchange" && <ExchangePage />}
-            {page === "setup-currencies" && <SetupCurrencies />}
-            {page === "setup-exhange-rates" && <SetupExchangeRates />}
-          </Card>
-        </Box>
+      <AppBar position="static">
+        <Tabs
+          value={page}
+          onChange={(_, x) => setPage(x)}
+          centered
+          centerRipple
+        >
+          <Tab label="Setup Currencies" />
+          <Tab label="Exchange" />
+          <Tab label="Setup Exchange Rates" />
+        </Tabs>
+      </AppBar>
+
+      <Box marginTop={5} flex={1} minWidth={"75%"}>
+        <Card>
+          {page === 0 && <SetupCurrencies />}
+          {page === 1 && <ExchangePage />}
+          {page === 2 && <SetupExchangeRates />}
+        </Card>
       </Box>
     </DBContext.Provider>
   );
